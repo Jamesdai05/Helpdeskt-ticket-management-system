db4a698fb99bedb765787671eb2c4b58a6ce900f
jamesdai05 <dai_guilong@hotmail.com>
2026-09-14 17:44:39 +0800

    feat:add the getting current user function

---
+++ src/lib/currentUser.ts
@@ -0,0 +1,33 @@
+import { verifyAuthToken, getAuthCookie } from "./auth";
+import { prisma } from "@/db/prisma";
+
+
+type AuthPayload = {
+    userId: string;
+};
+
+
+const getCurrentUser = async () => {
+    try {
+        const token = await getAuthCookie();
+        if (!token) return null;
+
+        const payload = await verifyAuthToken<AuthPayload>(token);
+
+        if (!payload?.userId) return null;
+
+        const currentUser = await prisma.user.findUnique({
+            where: { id: payload.userId },
+            // select the data
+            select: {
+                id: true,
+                email: true,
+                name:true,
+            },
+        })
+        return currentUser;
+    } catch (error) {
+        console.error("Error in getting the current user!",error);
+        return null;
+    }
+}
